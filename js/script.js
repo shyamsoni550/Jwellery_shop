037f2354c6f762b61c5f84bd6044e17ceea679d2
shyam vaya <vayashyam550@gmail.com>
2024-07-25 20:20:30 +0530

    creating a full stack jwelery shop

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-function sortProducts(order) {
-     let products = document.querySelectorAll('.product');
-     // Sort products based on price (ascending or descending)
-     products.sort((a, b) => {
-       const priceA = parseFloat(a.querySelector('p').textContent.replace('$', ''));
-       const priceB = parseFloat(b.querySelector('p').textContent.replace('$', ''));
-       return order === 'asc' ? priceA - priceB : priceB - priceA;
-     });
-     // Update DOM with sorted products
-     document.querySelector('.products').appendChild(...products);
-   }
+// function sortProducts(order) {
+//      let products = document.querySelectorAll('.product');
+//      // Sort products based on price (ascending or descending)
+//      products.sort((a, b) => {
+//        const priceA = parseFloat(a.querySelector('p').textContent.replace('$', ''));
+//        const priceB = parseFloat(b.querySelector('p').textContent.replace('$', ''));
+//        return order === 'asc' ? priceA - priceB : priceB - priceA;
+//      });
+//      // Update DOM with sorted products
+//      document.querySelector('.products').appendChild(...products);
+//    }
    
-   // Add event listeners to sort buttons
-   document.getElementById('sortAsc').addEventListener('click', () => sortProducts('asc'));
-   document.getElementById('sortDesc').addEventListener('click', () => sortProducts('desc'));
+//    // Add event listeners to sort buttons
+//    document.getElementById('sortAsc').addEventListener('click', () => sortProducts('asc'));
+//    document.getElementById('sortDesc').addEventListener('click', () => sortProducts('desc'));
    
+const panels=document.querySelectorAll('.panel')
+panels.forEach(panel=>{
+     panel.addEventListener('click',()=>{
+          removeActiveClasses()
+          panel.classList.add('active')
+     })
+})
+
+ function removeActiveClasses() {
+     panels.forEach(panel=>{
+          panel.classList.remove('active')
+     })
+ }
